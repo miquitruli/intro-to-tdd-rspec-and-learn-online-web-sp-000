a57027c2006e1e3f82807638c2ef8d61a47b67c7
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-04-14 23:10:15 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,3 +1,4 @@
+def
 require_relative './current_age_for_birth_year.rb'
 
 puts "What year were you born?"
@@ -6,3 +7,4 @@ birth_year = gets.to_i
 users_age = current_age_for_birth_year(birth_year)
 
 puts "You are: " + users_age.to_s + " years old."
+end
